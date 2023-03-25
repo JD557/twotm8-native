2297bdc2a96184eb4ad10894840d093b1924153c
João Costa <jdpc557@gmail.com>
2023-03-25 14:15:32 +0100

    Add fallback BinCurlHttpClient

---
+++ src/main/scala/eu/joaocosta/twotm8native/BinCurlHttp.scala
@@ -0,0 +1,12 @@
+package eu.joaocosta.twotm8native
+
+import scala.io.Source
+import scala.scalanative.libc.stdlib
+import scala.scalanative.unsafe.*
+
+object BinCurlHttp extends Http {
+  def get(url: String): String = Zone { implicit z =>
+    stdlib.system(toCString(s"curl $url > ${Constants.tmpFile}"))
+    Source.fromFile(Constants.tmpFile).getLines().mkString("\n")
+  }
+}
